20ce7cbb65c72596f71f006b3d7ecd211867971e
Bruno Carlini <bruno-carlini@hotmail.com>
2022-04-18 15:33:20 -0300

    Aula 7 e 8 Assistida

---
+++ aula07/aritmeticos.js
@@ -0,0 +1,26 @@
+/*
+Adição +
+Subtração -
+Divisão /
+Multiplicação *
+Modulo/resto %
+Exponenciação **
+
+Incremento:
+var num
+num++
+
+(Incremento:
+    Operador unário. Adiciona um ao seu operando. 
+    Se usado como operador prefixado (++x), retorna o valor de seu operando após a adição. 
+    Se usado como operador pósfixado (x++),
+    retorna o valor de seu operando antes da adição.
+
+)
+
+Decremento:
+var num
+num--
+
+
+*/
---
+++ aula08/relacionais_e_logicos.js
@@ -0,0 +1,14 @@
+/*
+Operadores Relacionais:
+1. > Maior
+2. < Menor
+3. >= Maior igual
+4. <= Menor igual
+5. == Igualdade
+6. != Diferente de
+
+
+Operadores Lógicos:
+1. True = Verdadeiro
+2. False = Falso
+*/
