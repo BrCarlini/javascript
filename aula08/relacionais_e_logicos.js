57f3c433b8ae4d48316509d3b2ae5a13db5ce238
Bruno Carlini <bruno-carlini@hotmail.com>
2022-04-18 16:12:24 -0300

    Att Aula 8

--- aula08/relacionais_e_logicos.js
+++ aula08/relacionais_e_logicos.js
@@ -4,11 +4,18 @@ Operadores Relacionais:
 2. < Menor
 3. >= Maior igual
 4. <= Menor igual
-5. == Igualdade
+5. == Igual que
 6. != Diferente de
+7. === Restritamente igual a
+8. !== Desigual restrito
+================================
+1. True = Verdadeiro
+2. False = Falso
+================================
 
 
 Operadores Lógicos:
-1. True = Verdadeiro
-2. False = Falso
+! Negação (not)
+&& Conjunção (and)
+|| disjunção (ou)
 */
